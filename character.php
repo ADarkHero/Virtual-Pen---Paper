92fb02402151c4f7d89f19bd79c143deec100cd4
ADarkHero <maik.riedlsperger@gmail.com>
2018-04-27 14:46:05 +0200

    Added character pictures. Fixes #14

--- character.php
+++ character.php
@@ -2,139 +2,162 @@
 session_start();
 require_once("inc/config.inc.php");
 require_once("inc/functions.inc.php");
+require_once("inc/character/character.inc.php");
 include("templates/header.inc.php");
-
-function weapongenerator($row, $currentrow) {
-    $weapons = scandir("img/weapon"); //Path with all weapons
-    foreach ($weapons as &$weapon) {
-        echo "<option";
-        if ($row[$currentrow] == $weapon) {
-            echo " selected";
-        }
-        echo " value='" . $weapon . "'>" . substr($weapon, 0, -4) . "</option>";    //Cut .svg away
-    }
-}
-
-$readonly = true;
-
-if (isset($user['id'])) { //User logged in?
-    if (isset($_GET['id'])) {     //User wants to view/edit a character
-        $id = $_GET['id'];
-        $statement = $pdo->prepare("SELECT account FROM characters WHERE id = $id");
-        $result = $statement->execute();
-        while ($row = $statement->fetch()) {
-            if ($row['account'] == $user['id']) { //User can only edit his own characters
-                $readonly = false;
-                break;
-            }
-        }
-    } else {
-        $readonly = false;
-    }
-}
-
-if ($readonly == true) {
-    echo "Read only mode.";
-}
 ?>
+<div class="container-fluid">
+    <?php
+    $readonly = true;
+
+    if (isset($user['id'])) { //User logged in?
+        if (isset($_GET['id'])) {     //User wants to view/edit a character
+            $id = $_GET['id'];
+            $statement = $pdo->prepare("SELECT account FROM characters WHERE id = $id");
+            $result = $statement->execute();
+            while ($row = $statement->fetch()) {
+                if ($row['account'] == $user['id']) { //User can only edit his own characters
+                    $readonly = false;
+                    break;
+                }
+            }
+        } else {
+            $readonly = false;
+        }
+    }
+
+    if ($readonly == true) {
+        echo "Read only mode.";
+    }
+    ?>
 
 
-<?php
+    <?php
 //Is a new character created?		
-if (isset($_GET["new"])) {
-    echo "<p>Creating new character.</p>";
-    $id = 0;
-}
+    if (isset($_GET["new"])) {
+        echo "<p>Creating new character.</p>";
+        $id = 0;
+    }
 
-if (isset($_GET["id"])) {
-    $id = $_GET["id"];
-}
+    if (isset($_GET["id"])) {
+        $id = $_GET["id"];
+    }
 
 //Was the formular used?
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 //Let's build an sql statement!
 
-    $sql = "";
+        $sql = "";
 
-    // Build insert into statement	
-    if ($id == 0) {
-        $sql = $sql . "INSERT INTO characters (account, ";
+        // Build insert into statement	
+        if ($id == 0) {
+            $sql = $sql . "INSERT INTO characters (account, ";
 
-        foreach ($_POST as $key => $value) {
-            $sql = $sql . "" . $key . ", ";
+            foreach ($_POST as $key => $value) {
+                $sql = $sql . "" . $key . ", ";
+            }
+
+            $sql = substr($sql, 0, -10); //Cut of the last comma and the submit
+
+            $sql = $sql . ") VALUES (" . $_SESSION['userid'] . ", ";
+
+            foreach ($_POST as $key => $value) {
+                $sql = $sql . "'" . $value . "', ";
+            }
+
+            $sql = substr($sql, 0, -12); //Cut of the last comma and the 'Submit'
+
+            $sql = $sql . ")";
+        }
+        //Build update statement
+        else {
+            $sql = $sql . "UPDATE characters SET ";
+            foreach ($_POST as $key => $value) {
+                $sql = $sql . $key . " = '" . $value . "', ";
+            }
+            $sql = substr($sql, 0, -21); //Cut of the last comma and the submit stuff
+
+            $sql = $sql . " WHERE ID = " . $id;
         }
 
-        $sql = substr($sql, 0, -10); //Cut of the last comma and the submit
+        $statement = $pdo->prepare($sql);
+        $result = $statement->execute();
 
-        $sql = $sql . ") VALUES (" . $_SESSION['userid'] . ", ";
-
-        foreach ($_POST as $key => $value) {
-            $sql = $sql . "'" . $value . "', ";
-        }
-
-        $sql = substr($sql, 0, -12); //Cut of the last comma and the 'Submit'
-
-        $sql = $sql . ")";
+        updatePictures("mainPicture", $pdo, $id);
+        updatePictures("secondaryPicture", $pdo, $id);
+        updatePictures("tertiaryPicture", $pdo, $id);
     }
-    //Build update statement
-    else {
-        $sql = $sql . "UPDATE characters SET ";
-        foreach ($_POST as $key => $value) {
-            $sql = $sql . $key . " = '" . $value . "', ";
-        }
-        $sql = substr($sql, 0, -21); //Cut of the last comma and the submit stuff
-
-        $sql = $sql . " WHERE ID = " . $id;
-    }
-
-
-    $statement = $pdo->prepare($sql);
-    $result = $statement->execute();
-}
-
 //Read character data from database
-$statement = $pdo->prepare("SELECT * FROM characters WHERE id = $id");
-$result = $statement->execute();
-$count = 1;
+    $statement = $pdo->prepare("SELECT * FROM characters WHERE id = $id");
+    $result = $statement->execute();
+    $count = 1;
 
-while ($row = $statement->fetch()) {
-    ?>
-    <div class="container-fluid">
+    while ($row = $statement->fetch()) {
+        ?>
 
-        <form action="" method="post">
+
+        <form action="" method="post" enctype="multipart/form-data">
             <!-- PICTURES -->
             <div class="form-group row">
-                <div class="col-md-6 form">
-                    TODO: Character-Picture
+                <div class="col-md-2 form" data-toggle="tooltip" title="It's your character!">
+                    <?php
+                    if ($row['mainPicture'] !== "") {
+                        echo '<span class="characterpicture"><center><img src="img/character/' . $row['mainPicture'] . '" data-toggle="tooltip"></img></center></span>';
+                    }
+                    ?>
+                </div>
+                <div class="col-md-2 form" data-toggle="tooltip" title="It's your character!">
+                    <?php
+                    if ($row['secondaryPicture'] !== "") {
+                        echo '<span class="characterpicture"><center><img src="img/character/' . $row['secondaryPicture'] . '" data-toggle="tooltip" title="It\'s your character!"></img></center></span>';
+                    }
+                    ?>
+                </div>
+                <div class="col-md-2 form" data-toggle="tooltip" title="It's your character!">
+                    <?php
+                    if ($row['tertiaryPicture'] !== "") {
+                        echo '<span class="characterpicture"><center><img src="img/character/' . $row['tertiaryPicture'] . '" data-toggle="tooltip" title="It\'s your character!"></img></center></span>';
+                    }
+                    ?>
                 </div>
                 <div class="col-md-1 form">
                     <?php
                     if ($row['weapon'] !== "") {
-                        echo '<span id="weapon" class="weaponimage"><img src="img/weapon/' . $row['weapon'] . '" data-toggle="tooltip" title="Weapon: ' . substr($row['weapon'], 0, -4) . '"></img></span>';
+                        echo '<span id="weapon" class="weaponimage"><center><img src="img/weapon/' . $row['weapon'] . '" data-toggle="tooltip" title="Weapon: ' . substr($row['weapon'], 0, -4) . '"></img></center></span>';
                     }
                     echo '<br />';
                     if ($row['subweapon'] !== "") {
-                        echo '<span id="subweapon" class="weaponimage"><img src="img/weapon/' . $row['subweapon'] . '" data-toggle="tooltip" title="Subweapon: ' . substr($row['subweapon'], 0, -4) . '"></img></span>';
+                        echo '<span id="subweapon" class="weaponimage"><center><img src="img/weapon/' . $row['subweapon'] . '" data-toggle="tooltip" title="Subweapon: ' . substr($row['subweapon'], 0, -4) . '"></img></center></span>';
                     }
                     ?>
                 </div>
             </div>
 
+            <!-- PICTURE UPLOAD -->
+            <div class="form-group row">
+                <div class="col-md-2 form" data-toggle="tooltip" title="Picture upload">
+                    <input class="form-control" value="Picture upload" disabled>
+                </div>
+                <div class="col-md-4 form" data-toggle="tooltip" title="Main picture">
+                    <input class="form-control" type="file" name="mainPicture">
+                </div>
+                <div class="col-md-3 form" data-toggle="tooltip" title="Secondary picture">
+                    <input class="form-control" type="file" name="secondaryPicture">
+                </div>
+                <div class="col-md-3 form" data-toggle="tooltip" title="Tertiary picture">
+                    <input class="form-control" type="file" name="tertiaryPicture">
+                </div>     
+            </div>
 
             <!-- NAME -->
             <div class="form-group row">
-                <div class="col-md-2 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="First Name">
-                        <input class="form-control" value="First Name" disabled>
-                    </span>
+                <div class="col-md-2 form" data-toggle="tooltip" title="First Name">
+                    <input class="form-control" value="First Name" disabled>
                 </div>
                 <div class="col-md-4"><input type="text" name="firstName" class="form-control" placeholder="First Name" title="First Name" value="<?php echo $row['firstName']; ?>"  autofocus></div>
 
-                <div class="col-md-2 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Last Name">
-                        <input class="form-control" value="Last Name" disabled>
-                    </span>
+                <div class="col-md-2 form" data-toggle="tooltip" title="Last Name">
+                    <input class="form-control" value="Last Name" disabled>
                 </div>
                 <div class="col-md-4"><input type="text" name="lastName" class="form-control" placeholder="Last Name" value="<?php echo $row['lastName']; ?>"  ></div>
             </div>
@@ -149,10 +172,8 @@ while ($row = $statement->fetch()) {
             ?>
 
             <div class="form-group row">
-                <div class="col-md-1 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Sex">
-                        <input class="form-control" value="Sex" disabled>
-                    </span>
+                <div class="col-md-1 form" data-toggle="tooltip" title="Sex">
+                    <input class="form-control" value="Sex" disabled>
                 </div>
                 <div class="col-md-3"><select name="sex" id="sex" class="form-control" >
                         <?php
@@ -166,17 +187,13 @@ while ($row = $statement->fetch()) {
                         ?>
                     </select>
                 </div>
-                <div class="col-md-1 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Birthday">
-                        <input class="form-control" value="Birthday" disabled>
-                    </span>
+                <div class="col-md-1 form" data-toggle="tooltip" title="Birthday">
+                    <input class="form-control" value="Birthday" disabled>
                 </div>
                 <div class="col-md-3"><input type="text" name="birthday" class="form-control" placeholder="Birthday"  value="<?php echo $row['birthday']; ?>"  ></div>
 
-                <div class="col-md-1 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Age">
-                        <input class="form-control" value="Age" disabled>
-                    </span>
+                <div class="col-md-1 form" data-toggle="tooltip" title="Age">
+                    <input class="form-control" value="Age" disabled>
                 </div>
                 <div class="col-md-3"><input type="text" name="age" class="form-control" placeholder="Age" value="<?php echo $row['age']; ?>"  ></div>
             </div>
@@ -185,31 +202,23 @@ while ($row = $statement->fetch()) {
 
             <!-- WEIGHT, SIZE, EYES, HAIR -->	
             <div class="form-group row">
-                <div class="col-md-1 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Weight">
-                        <input class="form-control" value="Weight" disabled>
-                    </span>
+                <div class="col-md-1 form" data-toggle="tooltip" title="Weight">
+                    <input class="form-control" value="Weight" disabled>
                 </div>
                 <div class="col-md-2"><input type="text" name="weight" class="form-control" placeholder="Weight" value="<?php echo $row['weight']; ?>"  ></div>
 
-                <div class="col-md-1 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Size">
-                        <input class="form-control" value="Size" disabled>
-                    </span>
+                <div class="col-md-1 form" data-toggle="tooltip" title="Size">
+                    <input class="form-control" value="Size" disabled>
                 </div>
                 <div class="col-md-2"><input type="text" name="size" class="form-control" placeholder="Size" value="<?php echo $row['size']; ?>"  ></div>	
 
-                <div class="col-md-1 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Eyecolor">
-                        <input class="form-control" value="Eyecolor" disabled>
-                    </span>
+                <div class="col-md-1 form" data-toggle="tooltip" title="Eyecolor">
+                    <input class="form-control" value="Eyecolor" disabled>
                 </div>
                 <div class="col-md-2"><input type="text" name="eyes" class="form-control" placeholder="Eyecolor"  value="<?php echo $row['eyes']; ?>"  ></div>
 
-                <div class="col-md-1 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Hairstyle">
-                        <input class="form-control" value="Hairstyle" disabled>
-                    </span>
+                <div class="col-md-1 form" data-toggle="tooltip" title="Hairstyle">
+                    <input class="form-control" value="Hairstyle" disabled>
                 </div>
                 <div class="col-md-2"><input type="text" name="hair" class="form-control" placeholder="Hairstyle"  value="<?php echo $row['hair']; ?>"  ></div>
             </div>
@@ -223,10 +232,8 @@ while ($row = $statement->fetch()) {
             $races = $statement->fetchAll();
             ?>
             <div class="form-group row">
-                <div class="col-md-2 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Race">
-                        <input class="form-control" value="Race" disabled>
-                    </span>
+                <div class="col-md-2 form" data-toggle="tooltip" title="Race">
+                    <input class="form-control" value="Race" disabled>
                 </div>
                 <div class="col-md-4">
                     <select name="race" id="race" class="form-control" >
@@ -242,10 +249,8 @@ while ($row = $statement->fetch()) {
                         ?>
                     </select>
                 </div>
-                <div class="col-md-2 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Subrace">
-                        <input class="form-control" value="Subrace" disabled>
-                    </span>
+                <div class="col-md-2 form" data-toggle="tooltip" title="Subrace">
+                    <input class="form-control" value="Subrace" disabled>
                 </div>
                 <div class="col-md-4">
                     <select name="subrace" class="form-control" >
@@ -271,10 +276,8 @@ while ($row = $statement->fetch()) {
             $classes = $statement->fetchAll();
             ?>
             <div class="form-group row">
-                <div class="col-md-2 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Class">
-                        <input class="form-control" value="Class" disabled>
-                    </span>
+                <div class="col-md-2 form" data-toggle="tooltip" title="Class">
+                    <input class="form-control" value="Class" disabled>
                 </div>
                 <div class="col-md-4">
                     <select name="class" id="class" class="form-control" >
@@ -290,10 +293,8 @@ while ($row = $statement->fetch()) {
                         ?>
                     </select>
                 </div>
-                <div class="col-md-2 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Subclass">
-                        <input class="form-control" value="Subclass" disabled>
-                    </span>
+                <div class="col-md-2 form" data-toggle="tooltip" title="Subclass">
+                    <input class="form-control" value="Subclass" disabled>
                 </div>
                 <div class="col-md-4">
                     <select name="subclass" class="form-control" >
@@ -314,10 +315,8 @@ while ($row = $statement->fetch()) {
 
             <!-- WEAPON -->
             <div class="form-group row">
-                <div class="col-md-2 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Weapon">
-                        <input class="form-control" value="Weapon" disabled>
-                    </span>
+                <div class="col-md-2 form" data-toggle="tooltip" title="Weapon">
+                    <input class="form-control" value="Weapon" disabled>
                 </div>
                 <div class="col-md-4">
                     <select name="weapon" class="form-control" onchange='changeWeapon(this.value, "weapon")'>
@@ -326,10 +325,8 @@ while ($row = $statement->fetch()) {
                         ?>
                     </select>
                 </div>
-                <div class="col-md-2 form">
-                    <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Subweapon">
-                        <input class="form-control" value="Subweapon" disabled>
-                    </span>
+                <div class="col-md-2 form" data-toggle="tooltip" title="Subweapon">
+                    <input class="form-control" value="Subweapon" disabled>
                 </div>
                 <div class="col-md-4">
                     <select name="subweapon" class="form-control" onchange='changeWeapon(this.value, "subweapon")'>
