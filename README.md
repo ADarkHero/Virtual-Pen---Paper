40b3dd9ced67c1079a200887f00ab4107ccf8e42
ADarkHero <maik.riedlsperger@gmail.com>
2018-04-30 13:10:58 +0200

    Updated readme and sql-dump

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Virtual-Pen-and-Paper
+![Virtual-Pen-and-Paper Screenshot](https://i.imgur.com/cTkydP9.jpg)
 A digital version of a pen and paper rpg.
+
+## Small tutorial, to get this project running on your server:
+* Download the [latest release](https://github.com/ADarkHero/Virtual-Pen-and-Paper/releases)
+* Put in on your **PHP-server** (5.6.32 recommended)
+* Load the **mysql dump** (sqldump-30-04-2018.sql), or create your own database (see below)
+* Change the variables in the **inc/config.inc.php**.
--- sqldump-30-04-2018.sql
+++ sqldump-30-04-2018.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2018 at 03:56 PM
+-- Generation Time: Apr 30, 2018 at 12:57 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 5.6.32
 
@@ -32,28 +32,54 @@ CREATE TABLE `characters` (
   `id` int(11) NOT NULL,
   `account` int(11) NOT NULL,
   `firstName` text NOT NULL,
+  `title` varchar(255) NOT NULL,
   `lastName` text NOT NULL,
-  `sex` int(11) NOT NULL,
+  `sex` varchar(10) NOT NULL,
   `birthday` text NOT NULL,
   `age` int(11) NOT NULL,
   `weight` int(11) NOT NULL,
   `size` int(11) NOT NULL,
-  `eyes` text NOT NULL,
-  `hair` text NOT NULL,
-  `race` int(11) NOT NULL,
-  `subrace` int(11) NOT NULL,
-  `class` int(11) NOT NULL,
-  `subclass` int(11) NOT NULL
+  `eyes` varchar(255) NOT NULL,
+  `hair` varchar(255) NOT NULL,
+  `race` varchar(255) NOT NULL,
+  `subrace` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `subclass` varchar(255) NOT NULL,
+  `weapon` varchar(255) NOT NULL,
+  `subweapon` varchar(255) NOT NULL,
+  `mainPicture` varchar(255) NOT NULL,
+  `secondaryPicture` varchar(255) NOT NULL,
+  `tertiaryPicture` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `courage` int(11) NOT NULL,
+  `wisdom` int(11) NOT NULL,
+  `intuition` int(11) NOT NULL,
+  `charisma` int(11) NOT NULL,
+  `dexterity` int(11) NOT NULL,
+  `agility` int(11) NOT NULL,
+  `physique` int(11) NOT NULL,
+  `strenght` int(11) NOT NULL,
+  `lpmax` int(11) NOT NULL,
+  `lpcurrent` int(11) NOT NULL,
+  `manamax` int(11) NOT NULL,
+  `manacurrent` int(11) NOT NULL,
+  `experience` int(11) NOT NULL,
+  `unusedExperience` int(11) NOT NULL,
+  `fightingTalents` text NOT NULL,
+  `bodyTalents` text NOT NULL,
+  `societyTalents` text NOT NULL,
+  `natureTalents` text NOT NULL,
+  `knowledgeTalents` text NOT NULL,
+  `craftTalents` text NOT NULL,
+  `publicEntry` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `characters`
 --
 
-INSERT INTO `characters` (`id`, `account`, `firstName`, `lastName`, `sex`, `birthday`, `age`, `weight`, `size`, `eyes`, `hair`, `race`, `subrace`, `class`, `subclass`) VALUES
-(0, 0, '', '', 0, '', 0, 0, 0, '', '', 0, 0, 0, 0),
-(4, 0, 'asdfasdasdfasdf', '', 0, '', 0, 0, 0, '', '', 0, 0, 0, 0),
-(5, 2, '', '', 0, '', 0, 0, 0, '', '', 0, 0, 0, 0);
+INSERT INTO `characters` (`id`, `account`, `firstName`, `title`, `lastName`, `sex`, `birthday`, `age`, `weight`, `size`, `eyes`, `hair`, `race`, `subrace`, `class`, `subclass`, `weapon`, `subweapon`, `mainPicture`, `secondaryPicture`, `tertiaryPicture`, `description`, `courage`, `wisdom`, `intuition`, `charisma`, `dexterity`, `agility`, `physique`, `strenght`, `lpmax`, `lpcurrent`, `manamax`, `manacurrent`, `experience`, `unusedExperience`, `fightingTalents`, `bodyTalents`, `societyTalents`, `natureTalents`, `knowledgeTalents`, `craftTalents`, `publicEntry`) VALUES
+(0, 0, '', '', '', '', '', 18, 80, 180, '', '', '', '', '', '', '', '', '', '', '', '', 10, 10, 10, 10, 10, 10, 10, 10, 20, 20, 0, 0, 500, 500, '', '', '', '', '', '', 'true');
 
 -- --------------------------------------------------------
 
@@ -71,8 +97,32 @@ CREATE TABLE `classes` (
 --
 
 INSERT INTO `classes` (`id`, `name`) VALUES
-(1, 'asdf'),
-(2, 'asdfasdfas');
+(0, 'None');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `groupname` text NOT NULL,
+  `account` text NOT NULL,
+  `mainPicture` varchar(255) NOT NULL,
+  `secondaryPicture` varchar(255) NOT NULL,
+  `tertiaryPicture` varchar(255) NOT NULL,
+  `publicEntry` varchar(10) NOT NULL,
+  `members` text,
+  `onlyAdminMode` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `groupname`, `account`, `mainPicture`, `secondaryPicture`, `tertiaryPicture`, `publicEntry`, `members`, `onlyAdminMode`) VALUES
+(0, '', '', '', '', '', 'true', '', 'true');
 
 -- --------------------------------------------------------
 
@@ -90,8 +140,7 @@ CREATE TABLE `races` (
 --
 
 INSERT INTO `races` (`id`, `name`) VALUES
-(1, 'test'),
-(2, 'asdfta');
+(0, 'None');
 
 -- --------------------------------------------------------
 
@@ -106,6 +155,11 @@ CREATE TABLE `securitytokens` (
   `securitytoken` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `securitytokens`
+--
+
 
 -- --------------------------------------------------------
 
@@ -123,7 +177,9 @@ CREATE TABLE `sex` (
 --
 
 INSERT INTO `sex` (`ID`, `name`) VALUES
-(1, 'test');
+(1, 'male'),
+(2, 'female'),
+(3, 'other');
 
 -- --------------------------------------------------------
 
@@ -143,7 +199,6 @@ CREATE TABLE `users` (
 --
 
 
-
 --
 -- Indexes for dumped tables
 --
@@ -155,10 +210,22 @@ ALTER TABLE `characters`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `securitytokens`
 --
 ALTER TABLE `securitytokens`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sex`
+--
+ALTER TABLE `sex`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indexes for table `users`
@@ -175,19 +242,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `characters`
 --
 ALTER TABLE `characters`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `securitytokens`
 --
 ALTER TABLE `securitytokens`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
